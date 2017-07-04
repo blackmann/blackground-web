eb9589263a8ccfa51676c1e7bb0523582841dd9e
De-Great Yartey <yarteydegreat2@gmail.com>
2017-07-04 17:27:47 +0000

    Corruption occurred. Checkpoint.

--- src/views/MainBody.jsx
+++ src/views/MainBody.jsx
@@ -1,15 +1,8 @@
 import React, {Component} from "react";
-import StackGrid from "react-stack-grid";
-import Graphic from "../components/Graphic";
+import PhotoGrid from "../components/PhotoGrid";
 import graphics from "../resources/graphics";
 
 class MainBody extends Component {
-
-    renderGallery() {
-        return graphics.map((item, index) => {
-            return <Graphic imageSrc={item.image} key={index}/>
-        })
-    }
 
     render() {
         return (
@@ -19,9 +12,7 @@ class MainBody extends Component {
                     <p className="support-text">Let us do it right for you. And be surprised.</p>
                 </div>
 
-                <StackGrid columnWidth="50%" gutterWidth={10} className="gallery-grid">
-                    {this.renderGallery()}
-                </StackGrid>
+                <PhotoGrid photos={graphics}/>
             </div>
         )
     }
