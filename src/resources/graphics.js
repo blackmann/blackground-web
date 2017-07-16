53f1d0269d18f7725f20061ae638b3e1de1d7f16
De-Great Yartey <yarteydegreat2@gmail.com>
2017-07-16 00:32:20 +0000

    Added new logos

--- src/resources/graphics.js
+++ src/resources/graphics.js
@@ -1,28 +1,28 @@
-import book_illustration from "../gallery/book_illustration.jpg";
-import photography_1  from "../gallery/photography_1.jpg";
-import workspace_2 from "../gallery/workspace_2.jpg";
-let workspace_1 = require("../gallery/workspace_1.jpg");
+import afabea1 from "../gallery/afabea_logo-1-01.jpg";
+import afabea2 from "../gallery/afabea_logo-2-01.jpg";
+import afabea3 from "../gallery/logo-01.jpg";
+import braperucci from "../gallery/braperucci_logo-01.jpg";
+import arteases from "../gallery/arteases_logo-01.jpg";
+import dogo from "../gallery/dogo-logo-01.jpg";
 
 let graphics = [
     {
-        title: "Book Illustration",
-        image: book_illustration,
-        comment: "Some literal comment"
+        image: afabea1
     },
     {
-        title: "Photography",
-        image: photography_1,
-        comment: "Some literal comment"
+        image: afabea2
     },
     {
-        title: "Photography",
-        image: workspace_1,
-        comment: "Some literal comment"
+        image: afabea3
     },
     {
-        title: "Photography",
-        image: workspace_2,
-        comment: "Some literal comment"
+        image: braperucci
+    },
+    {
+        image: arteases
+    },
+    {
+        image: dogo
     }
 ];
 
